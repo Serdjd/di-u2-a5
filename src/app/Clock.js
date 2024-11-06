a00811617ee8d70fc203e24e1f289576aa909b9e
Serdjd <sergiodobladomunoz@gmail.com>
2024-11-06 10:56:33 +0100

    UPD: correct class to className

--- src/app/Clock.js
+++ src/app/Clock.js
@@ -1,10 +1,9 @@
 export default function Clock({ time }) {
-    let hours = time.getHours();
-    let name = (hours >= 0 && hours <= 6) ? 'night' : 'day'
-    return (
-      <h1 id="time" class={name}>
-        {time.toLocaleTimeString()}
-      </h1>
-    );
-  }
-  
+  let hours = time.getHours();
+  let name = (hours >= 0 && hours <= 6) ? 'night' : 'day'
+  return (
+    <h1 id="time" className={name}>
+      {time.toLocaleTimeString()}
+    </h1>
+  );
+}
